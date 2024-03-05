d60971b040056ae451c1e62a37b40be572fe2f0a
Hugo Pereira <128017769+Hugo8814@users.noreply.github.com>
2024-03-05 23:20:19 +0000

    boom

--- js/views/mainCardView.js
+++ js/views/mainCardView.js
@@ -28,26 +28,52 @@ class MainCardView extends View {
           <div class="right__box">
             <div class="country-card">
               <div class="country-card__info">
-                <div class="country-card__name">${country.nativeName}</div>
+                <div class="country-card__detail">Native Name: <span class="country-card__value">${
+                  country.nativeName
+                }</span></div>
                 <div class="country-card__detail">Population: <span class="country-card__value">${formatNumberWithCommas(
                   country.population
                 )}</span></div>
                 <div class="country-card__detail">Region: <span class="country-card__value">${
                   country.region
                 }</span></div>
+                <div class="country-card__detail">Sub Region: <span class="country-card__value">${
+                  country.subRegion
+                }</span></div>
                 <div class="country-card__detail">Capital: <span class="country-card__value">${
                   country.capital
                 }</span></div>
+                <div class="country-card__detail">Top Level Domain:<span class="country-card__value">${
+                  country.tld
+                }</span></div>
+                <div class="country-card__detail">Currencies: <span class="country-card__value">${Object.values(
+                  country.currencies
+                )
+                  .map((currency) => currency.name)
+                  .join(", ")}</span></div>
+                  <div class="country-card__detail">Languages: <span class="country-card__value">${Object.values(
+                    country.languages
+                  )
+                    .map((cur) => cur)
+                    .join(", ")}</span></div>
+
               </div>
             </div>
           </div>
           <div class="right__box-border">
-            <div class="right__box-border__text">
-              Border Countries:
-              <div class="right__box-border__text__countries">Lorem</div>
-              <div class="right__box-border__text__countries">Lorem</div>
-              <div class="right__box-border__text__countries">Lorem</div>
-            </div>
+          <div class="right__box-border__text">
+          Border Countries:
+          ${
+            country.borderCountries
+              ? country.borderCountries
+                  .map(
+                    (borderCountry) =>
+                      `<div class="right__box-border__text__countries">${borderCountry}</div>`
+                  )
+                  .join("")
+              : ""
+          }
+        </div>
           </div>
         </div>
       </div>
